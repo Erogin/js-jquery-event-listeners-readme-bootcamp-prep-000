55c2ecd319d336ca4f620d338527fb91c2f82bb3
Eric Roginek <eroginek@protonmail.com>
2017-10-08 01:05:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -25,7 +25,6 @@ function submitIt(){
   $("form").on("submit", function() {
     if ($( "input:first" ).val() === "correct") {
       alert('Your form is going to be submitted now.')
-      return
     }
   })
 }
